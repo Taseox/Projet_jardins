bd016fe42bee17f8bf05799a14efcc50d2ede7e8
Racine Jaïn <jain.racine@he-arc.ch>
2020-07-15 10:42:25 +0200

    Ajout de la template de base et des images des jardins

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 window.$ = $;
 import "bootstrap";
 //import "bootstrap/dist/css/bootstrap.min.css";
-import "bootswatch/dist/cerulean/bootstrap.min.css";
+import "bootswatch/dist/minty/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./main.css";
 import "jquery-smooth-scroll";
